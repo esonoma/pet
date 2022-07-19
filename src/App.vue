a2dca77ac29841378295cb9519f4bec626e08b61
lishiwen <lishiwen@wclound.cn>
2022-07-19 14:01:38 +0800

    feat: uni websocket example

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import { provide } from "vue";
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { useLogin } from "./store/login";
+
+const login = useLogin();
 
 onLaunch(() => {
-  // console.log("App Launch");
+  let wsConnected = false;
+  const socket = uni.connectSocket({
+    url: "ws://localhost:8080",
+    header: {
+      "content-type": "application/json",
+    },
+    success() {
+      wsConnected = true;
+    },
+  });
+  socket.onMessage((data: any) => {
+    login.setLoginUser(data.data);
+  });
+  provide("socket", socket);
+  provide("wsConnected", wsConnected);
 });
 onShow(() => {
   // console.log("App Show");
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,60 @@
 <template>
   <view class="content">
     <view class="text-area">
-      <text class="title">{{ title }} {{ login.username }}</text>
+      <text class="title"> {{ login.username }}</text>
     </view>
     <button type="button" @click="startLogin">Login</button>
     <button type="button" @click="loginOut">Logout</button>
   </view>
+
+  <view>
+    <view v-for="data in messages" :key="data">
+      <text>{{ data }}</text>
+    </view>
+  </view>
+  <input class="uni-input" placeholder="Username" v-model="to" />
+  <br />
+  <input class="uni-input" focus placeholder="消息内容" v-model="textMessage" />
+  <button type="button" @click="sendMessage">Send</button>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted, inject, reactive } from "vue";
 import { useLogin } from "../../store/login";
 
+const messages = reactive<string[]>([]);
+
+const to = ref("");
+const textMessage = ref("Hello");
+const socket: any = inject("socket");
+const wsConnected = inject("wsConnected");
+
 const login = useLogin();
-const title = ref("Hello");
+console.log(login.username);
+
+onMounted(() => {
+  if (wsConnected) {
+    socket.onMessage((data: any) => {
+      messages.push(data);
+    });
+  }
+});
+
+function sendMessage() {
+  if (wsConnected) {
+    socket.send({
+      data: JSON.stringify({
+        to: to.value,
+        text: textMessage.value,
+        type: "text",
+      }),
+    });
+  }
+}
 
 function startLogin() {
   login.setLoginUser("XMing");
 }
-
 function loginOut() {
   login.logout();
 }
