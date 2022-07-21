f44fa8bf61f5d11d8816ab1d62126fef15c60f1b
lishiwen <lishiwen@wclound.cn>
2022-07-22 01:56:49 +0800

    feat: universalKeys

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -2,3 +2,5 @@ export type AnyFunction = (...args: any[]) => any;
 export type AnyObject = { [key: string]: any };
 export type AnyArray = any[];
 export type AnyRecord = Record<string, any>;
+
+export type UniversalKeys = string | number | symbol;
