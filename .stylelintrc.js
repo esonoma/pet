f5c89f34d6fadc4fc39ca97fa52394f8a96d59bc
lishiwen <lishiwen@wclound.cn>
2022-07-23 01:52:27 +0800

    feat: guide go

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
 	root: true,
+	
 	extends: [
 		"stylelint-config-standard",
 		"stylelint-config-prettier",
 		"stylelint-config-recommended-vue",
 		"stylelint-config-standard-scss",
+		"stylelint-config-recommended"
 	],
 	rules: {
+		"indentation": "tab",
+		"unit-no-unknown": [true, { ignoreUnits: ["rpx", "upx"] }],
 		"media-feature-name-no-vendor-prefix": null,
 		"media-feature-name-no-unknown": null,
+		// "declaration-property-unit-whitelist": {
+		// 	"unprefixed-property-name": ["em", "rem", "%", "s", "px", "upx", "rpx"]
+		// },
+		// "unit-whitelist": ["em", "rem", "%", "s", "px", "upx", "rpx"],  
 	}
 };
---
+++ src/pages/guide/components/skip-guide/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<view @click="skipGuide"> <text>Close</text></view>
+</template>
+<script setup lang="ts">
+function skipGuide() {
+	//
+}
+</script>
