fb5630f3380dcc9dcc30d2f06d8266af24178fe2
lishiwen <lishiwen@wclound.cn>
2022-07-24 01:13:23 +0800

    feat(pages/index): home title

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,15 @@
 <template>
 	<app-header />
 	<view>
-		<text>Home</text>
+		<view class="title">
+			<text>Home</text>
+		</view>
 		<text> {{ $t("message.welcome") }} </text>
 	</view>
 </template>
 
 <script setup lang="ts">
-// import { reactive } from "vue";
 import appHeader from "@components/app-header/index.vue";
-// import WebsocketClient from "@message/WebsocketClient";
-
-// const messages = reactive<string[]>([]);
-// const websocketClient = new WebsocketClient({
-// 	url: "wss://ad24-222-211-237-49.jp.ngrok.io",
-// }).connect();
-// websocketClient.open().then(() => {
-// 	websocketClient.dispensers();
-// });
-
-// websocketClient.onTextMessage((message) => {
-// 	messages.push(JSON.stringify(message));
-// });
 </script>
 
 <style lang="less">
