b55b37d693721b9de324c8e104ec6d1fcecc6413
psj <271533323@qq.com>
2022-07-27 00:58:55 +0800

    feat: 完成登录页面

---
+++ src/pages/account/user-login/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="top">
+		123
+		<!-- <image class src="../../static/logo.png"></image> -->
+	</view>
+
+	<view class="login-form">
+		<uni-easyinput
+			prefixIcon="person-filled"
+			v-model="obj.userId"
+			placeholder="请输入账号"
+		></uni-easyinput>
+		<uni-easyinput
+			type="password"
+			prefixIcon="locked-filled"
+			v-model="obj.passWord"
+			placeholder="请输入密码"
+		/>
+		<view class="to-register" @click="gotoRegester"
+			>还没有账号？去注册></view
+		>
+	</view>
+
+	<view>
+		<button class="common-btn login-btn" @click="login">登录</button>
+		<button class="common-btn goto-home-btn" @click="gotoHome">
+			回到首页
+		</button>
+	</view>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const login = () => 0;
+const gotoHome = () => {
+	uni.redirectTo({ url: "/pages/index/index" });
+};
+
+const gotoRegester = () => {
+	uni.redirectTo({ url: "/pages/guide/guide" });
+};
+
+const obj = reactive({
+	userId: "",
+	passWord: "",
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
---
+++ src/pages/account/user-regester/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>
+		<text>注册</text>
+	</view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
