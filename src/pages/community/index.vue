08501f6b21422fd71275d15df1c26cef0dceda69
lishiwen <lishiwen@wclound.cn>
2022-07-27 20:04:50 +0800

    feat: 百分比布局使用案例

--- src/pages/community/index.vue
+++ src/pages/community/index.vue
@@ -1,6 +1,32 @@
 <template>
-	<view>
-		<text>Community</text>
+	<status-bar />
+	<view class="community-page-content">
+		<text>Community s</text>
 	</view>
 </template>
-<script lang="ts" setup></script>
+
+<script lang="ts" setup>
+import statusBar from "@components/status-bar/index.vue";
+</script>
+<style lang="scss">
+page {
+	height: 100%;
+	width: 100%;
+	background-color: salmon; // 鲑红
+}
+</style>
+
+<style lang="scss" scoped>
+// notes: 为什么page不生效? 参考下uni-app博文
+// ref: https://uniapp.dcloud.io/tutorial/syntax-css.html#%E9%80%89%E6%8B%A9%E5%99%A8
+page {
+	height: 100%;
+	width: 100%;
+	background-color: darkseagreen; // 暗海绿
+}
+
+.community-page-content {
+	height: 30%;
+	background-color: #c0caad; // 晨蓝
+}
+</style>
