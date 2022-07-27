2d8986d2dbe8949ba46ca27d3e39157e8ec44bce
lishiwen <lishiwen@wclound.cn>
2022-07-27 23:44:16 +0800

    feat: add http support for the base version

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -15,3 +15,14 @@ export function safeExecCallback<P1 = any, V = any>(
 export function noop() {
 	// do nothing
 }
+
+// 生成uuid
+export function createUuid() {
+	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+		// eslint-disable-next-line no-bitwise
+		const r = (Math.random() * 16) | 0;
+		// eslint-disable-next-line no-bitwise
+		const v = c === "x" ? r : (r & 0x3) | 0x8;
+		return v.toString(16);
+	});
+}
