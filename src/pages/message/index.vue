a4e840692814253069289cc8da76133dae8cee7e
lishiwen <lishiwen@wclound.cn>
2022-07-31 17:53:49 +0800

    feat: test the combination api of componstion and service

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,38 +1,11 @@
 <template>
 	<status-bar />
 	<app-header text="Message" page="Home" />
-	<view class="message-content">
-		<view
-			class="message-item-container"
-			v-for="message in messages"
-			:key="message.id"
-			:class="{
-				'is-self-message': true,
-			}"
-		>
-			{{ message.payload }}
-		</view>
-	</view>
+	<message-content />
 </template>
 
 <script lang="ts" setup>
 import statusBar from "@components/status-bar/index.vue";
 import appHeader from "@components/app-header/index.vue";
-import { onMounted, reactive } from "vue";
-
-import { getMessages } from "@domains/message.service";
-
-const messages = reactive([]);
-onMounted(async () => {
-	const data = await getMessages();
-	messages.push(...data.list);
-});
+import messageContent from "./message.vue";
 </script>
-<style scoped lang="scss">
-.message-content {
-	padding: 0rpx 30rpx;
-}
-.is-self-message {
-	text-align: right;
-}
-</style>
