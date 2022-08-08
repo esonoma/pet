6436e9a5a346533c2ec7b314657c5c9be732966a
lishiwen <lishiwen@wclound.cn>
2022-08-08 20:49:34 +0800

    feat: add app guidance function

--- src/pages/guide/components/guide-item/index.vue
+++ src/pages/guide/components/guide-item/index.vue
@@ -1,8 +1,40 @@
 <template>
 	<view class="guide-item">
-		<text>
-			{{ $props }}
-		</text>
+		<view class="guide-top-container">
+			<view class="responsive-image-container">
+				<img
+					v-if="data?.image?.src"
+					:src="data?.image?.src"
+					:alt="data?.image?.alt"
+				/>
+			</view>
+		</view>
+		<view class="guide-item-title">
+			<text> {{ data.title.content }} </text>
+		</view>
+		<view class="guide-item-description">
+			<text>{{ data.description }} </text>
+		</view>
+		<view class="guide-navigate-lines">
+			<view
+				class="guide-navigate-line-item"
+				v-for="line in size"
+				:key="data.id + line"
+				:class="{
+					'guide-navigate-line-item-active': active === line - 1,
+				}"
+			/>
+		</view>
+
+		<view class="guide-item-actions-container">
+			<button
+				type="button"
+				class="guide-item-action"
+				@tap="data.actions?.[0]?.onClick"
+			>
+				{{ data.actions[0].text }}
+			</button>
+		</view>
 	</view>
 </template>
 
@@ -28,10 +60,15 @@ defineProps({
 		required: false,
 		default: noop,
 	},
-	nativeProps: {
-		type: Object as PropType<object>,
+	size: {
+		type: Number as PropType<number>,
 		required: false,
-		default: () => ({}),
+		default: () => 0,
+	},
+	active: {
+		type: Number as PropType<number>,
+		required: false,
+		default: () => 0,
 	},
 });
 </script>
--- src/pages/guide/guide.vue
+++ src/pages/guide/guide.vue
@@ -4,32 +4,82 @@
 		keys="id"
 		class="swiper-guide-wrap"
 		:sources="guides"
-		:onLastItem="handlerLastItem"
 		easing-function="linear"
-		autoplay
+		:onChange="onChange"
+		:current="active"
 	>
 		<template #swiperItem="{ swiperItem, index }">
-			<guide-item :data="swiperItem" :index="index" />
+			<guide-item
+				:data="swiperItem"
+				:index="index"
+				:size="guides.length"
+				:active="active"
+			/>
 		</template>
 	</universal-swiper>
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
-import type { UniSwiperChangeEvent } from "@components/swiper/swiperTypes";
+import { reactive, ref } from "vue";
+import { onShow } from "@dcloudio/uni-app";
 import statusBar from "@/components/status-bar/index.vue";
 import universalSwiper from "@/components/swiper/swiper.vue";
 import guideItem from "./components/guide-item/index.vue";
+import type { UniSwiperChangeEvent } from "@/components/swiper/swiperTypes";
 
-const guides = reactive([
-	{ type: "text", id: "1", description: "A", bgColor: "red" },
-	{ type: "slot", id: "2", description: "B", bgColor: "yellow" },
-	{ type: "image", id: "3", description: "C", bgColor: "blue" },
-]);
-
-function handlerLastItem(event: UniSwiperChangeEvent) {
-	console.log("last item", event);
+onShow(() => {
+	if (uni.getStorageSync("guide_status")) {
+		uni.reLaunch({ url: "/pages/index/index" });
+	}
+});
+const active = ref(0);
+function nextGuidePage() {
+	active.value += 1;
 }
+const goHomePage = () => {
+	uni.reLaunch({ url: "/pages/index/index" });
+};
+const onChange = (event: UniSwiperChangeEvent) => {
+	active.value = event.detail.current;
+};
+const nextActions = [{ text: "Next", onClick: nextGuidePage }];
+const guides = reactive([
+	{
+		id: 0,
+		type: "rich",
+		image: {
+			src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=1.00xw:0.749xh;0,0.154xh&resize=980:*",
+			alt: "guide-item-image",
+		},
+		title: { content: "Find the Pet you love" },
+		description:
+			"Lorem ipsum dolor sit amet, consectetur  adipiscing elit. Lorem ",
+		actions: nextActions,
+	},
+	{
+		id: 1,
+		type: "rich",
+		image: {
+			src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=1.00xw:0.749xh;0,0.154xh&resize=980:*",
+			alt: "guide-item-image",
+		},
+		title: { content: "Find like-minded friends nearby" },
+		description: "Lorem ipsum dolor sit ame",
+		actions: nextActions,
+	},
+	{
+		id: 2,
+		type: "rich",
+		image: {
+			src: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=1.00xw:0.749xh;0,0.154xh&resize=980:*",
+			alt: "guide-item-image",
+		},
+		title: { content: "Find your new friends" },
+		description:
+			"Lorem ipsum dolor sit amet, consectetur  adipiscing elit. Lorem ",
+		actions: [{ text: "Get Started", onClick: goHomePage }],
+	},
+]);
 </script>
 
 <style lang="scss" scoped>
